eff0abf5fbf59546897d9ed74fb7bcc9297a98a4
sven3270350 <sven3270350@gmail.com>
2023-08-25 14:30:11 +0200

    fix introsection

--- components/common/InstroSection.tsx
+++ components/common/InstroSection.tsx
@@ -7,7 +7,10 @@ interface IntroSectionProps {
   activeSlide?: number;
   handleSlideToTwo?: () => void;
 }
-const IntroSection: React.FC<IntroSectionProps> = ({ handleSlideToTwo, activeSlide }) => {
+const IntroSection: React.FC<IntroSectionProps> = ({
+  handleSlideToTwo,
+  activeSlide,
+}) => {
   const { isTablet } = useMediaSize();
 
   return (
@@ -15,7 +18,10 @@ const IntroSection: React.FC<IntroSectionProps> = ({ handleSlideToTwo, activeSli
       <div className="max-w-[1560px] w-full">
         <div className="relative h-[100vh]">
           <Image
-            className={cx("absolute top-[20%] left-[8%]", activeSlide === 0 && "animate-normal-animation duration-1000")}
+            className={cx(
+              "absolute top-[20%] left-[8%]",
+              activeSlide === 0 && "animate-normal-animation duration-1000"
+            )}
             width={650}
             height={279.92}
             src="/assets/icons/LogoWhiteBlur.png"
@@ -23,11 +29,14 @@ const IntroSection: React.FC<IntroSectionProps> = ({ handleSlideToTwo, activeSli
           />
           <button
             type="button"
-            className={cx("absolute fuller-button blue top-[60%] left-[17%] rounded-3xl text-white flex justify-center items-center flex-shrink-0 w-[324px] py-2 px-6 font-normal text-base shadow-[0_4px_9px_-4px_#54b4d3] transition duration-150 ease-in-out hover:bg-info-600 hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:bg-info-600 focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:outline-none focus:ring-0 active:bg-info-700 active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(84,180,211,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)]", activeSlide === 0 && "animate-normal-animation duration-1000")}
+            className={cx(
+              "absolute fuller-button blue top-[60%] left-[17%] rounded-3xl text-white flex justify-center items-center flex-shrink-0 w-[324px] py-2 px-6 font-normal text-base shadow-[0_4px_9px_-4px_#54b4d3] transition duration-150 ease-in-out hover:bg-info-600 hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:bg-info-600 focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:outline-none focus:ring-0 active:bg-info-700 active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(84,180,211,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)]",
+              activeSlide === 0 && "animate-normal-animation duration-1000"
+            )}
           >
             Start Managing Today
           </button>
-          <div
+          {/* <div
             className={cx(
               "absolute text-white left-[10%] text-start",
               isTablet
@@ -38,6 +47,15 @@ const IntroSection: React.FC<IntroSectionProps> = ({ handleSlideToTwo, activeSli
           >
             Automated tool to easily manage, raise, {!isTablet && <br />}invest
             and trade cryptocurrency assets
+          </div> */}
+          <div
+            className={cx(
+              "absolute text-white left-[10%] text-start top-[75%] opacity-[.8] font-robotic text-4xl tracking-wide max-w-[668px]",
+              activeSlide === 0 && "animate-normal-animation duration-1000"
+            )}
+          >
+            Automated tool to easily manage, raise, invest and trade
+            cryptocurrency assets
           </div>
         </div>
         <div
