8654f12a5d8153f1f13fbf133e9197e28edefd58
sven3270350 <sven3270350@gmail.com>
2023-09-01 15:57:28 +0200

    afsdaafdsa

--- components/common/InstroSection.tsx
+++ components/common/InstroSection.tsx
@@ -24,16 +24,16 @@ const IntroSection: React.FC<IntroSectionProps> = ({
           <div
             className={cx(
               "w-full items-center flex flex-col h-[100vh] gap-10 justify-center max-w-[700px]",
-              isTablet && "!justify-start mt-32"
+              isTablet && "!justify-start"
             )}
           >
             <Image
               className={cx(
                 "mb-14",
                 activeSlide === 0 && "animate-normal-animation duration-1000",
-                isTablet && "hidden"
+                isTablet && "mb-0"
               )}
-              width={650}
+              width={isTablet ? 400 : 650}
               height={279.92}
               src="/assets/icons/LogoWhiteBlur.png"
               alt="IntroImage"
@@ -43,14 +43,14 @@ const IntroSection: React.FC<IntroSectionProps> = ({
               className={cx(
                 "fuller-button mb-32 blue rounded-3xl text-white flex justify-center items-center flex-shrink-0 w-[324px] py-2 px-6 font-normal text-base shadow-[0_4px_9px_-4px_#54b4d3] transition duration-150 ease-in-out hover:bg-info-600 hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:bg-info-600 focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:outline-none focus:ring-0 active:bg-info-700 active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(84,180,211,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)]",
                 activeSlide === 0 && "animate-normal-animation duration-1000",
-                isTablet && "hidden"
+                isTablet && "mb-0"
               )}
             >
               Start Managing Today
             </button>
             <div
               className={cx(
-                "text-white text-start opacity-[.8] font-robotic text-4xl tracking-wide max-w-[668px]",
+                "text-white opacity-[.8] font-robotic text-center text-4xl tracking-wide max-w-[668px]",
                 activeSlide === 0 && "animate-normal-animation duration-1000"
               )}
             >
