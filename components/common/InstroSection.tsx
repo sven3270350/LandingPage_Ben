3a6210a3b4c22f3edf438d47b19ecc1ad5ba7466
sven3270350 <sven3270350@gmail.com>
2023-09-01 14:13:44 +0200

    fix intro

--- components/common/InstroSection.tsx
+++ components/common/InstroSection.tsx
@@ -10,15 +10,18 @@ const IntroSection: React.FC<IntroSectionProps> = ({
   handleSlideToTwo,
   activeSlide,
 }) => {
-
   return (
     <div className="w-full bg-firstSection h-full flex justify-center">
       <div className="max-w-[1560px] w-full">
-        <div className="flex flex-wrap">
-          <div className={"relative w-full h-[100vh] max-w-[750px]"}>
+        <div className="flex flex-row justify-between items-center">
+          <div
+            className={
+              "w-full items-center flex flex-col h-[100vh] gap-10 justify-center max-w-[700px]"
+            }
+          >
             <Image
               className={cx(
-                "absolute top-[20%] left-[0%]",
+                "mb-14",
                 activeSlide === 0 && "animate-normal-animation duration-1000"
               )}
               width={650}
@@ -29,7 +32,7 @@ const IntroSection: React.FC<IntroSectionProps> = ({
             <button
               type="button"
               className={cx(
-                "absolute fuller-button blue top-[60%] left-[19%] rounded-3xl text-white flex justify-center items-center flex-shrink-0 w-[324px] py-2 px-6 font-normal text-base shadow-[0_4px_9px_-4px_#54b4d3] transition duration-150 ease-in-out hover:bg-info-600 hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:bg-info-600 focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:outline-none focus:ring-0 active:bg-info-700 active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(84,180,211,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)]",
+                "fuller-button mb-32 blue rounded-3xl text-white flex justify-center items-center flex-shrink-0 w-[324px] py-2 px-6 font-normal text-base shadow-[0_4px_9px_-4px_#54b4d3] transition duration-150 ease-in-out hover:bg-info-600 hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:bg-info-600 focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:outline-none focus:ring-0 active:bg-info-700 active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(84,180,211,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)]",
                 activeSlide === 0 && "animate-normal-animation duration-1000"
               )}
             >
@@ -37,7 +40,7 @@ const IntroSection: React.FC<IntroSectionProps> = ({
             </button>
             <div
               className={cx(
-                "absolute text-white left-[0%] text-start top-[75%] opacity-[.8] font-robotic text-4xl tracking-wide max-w-[668px]",
+                "text-white text-start opacity-[.8] font-robotic text-4xl tracking-wide max-w-[668px]",
                 activeSlide === 0 && "animate-normal-animation duration-1000"
               )}
             >
@@ -45,9 +48,19 @@ const IntroSection: React.FC<IntroSectionProps> = ({
               cryptocurrency assets
             </div>
           </div>
-          <div className="relative w-full max-w-[750px]">
-            <div className={cx("absolute top-[20%] left-[5%]",activeSlide === 0 && "animate-normal-animation duration-1000")}>
-              <video src="/assets/icons/Modules.mp4" width={750} autoPlay loop />
+          <div className="relative w-full max-w-[600px]">
+            <div
+              className={cx(
+                "",
+                activeSlide === 0 && "animate-normal-animation duration-1000"
+              )}
+            >
+              <video
+                src="/assets/icons/Modules.mp4"
+                width={600}
+                autoPlay
+                loop
+              />
             </div>
           </div>
         </div>
