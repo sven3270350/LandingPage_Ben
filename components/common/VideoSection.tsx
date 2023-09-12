178da95706d38ffd7ae021b7c6bd000cc325b4d8
secretsudev <secretsudev@gmail.com>
2023-09-12 16:28:20 +0200

    Fix asdfsa

--- components/common/VideoSection.tsx
+++ components/common/VideoSection.tsx
@@ -53,7 +53,7 @@ const VideoSection: React.FC<VideoSectionProps> = ({ activeSlide }) => {
           width={1187}
           height={684.5}
           alt="Introduction Video"
-          className="mb-5 w-full rounded-3xl"
+          className={cx("mb-5 w-full rounded-3xl", isTablet && "px-2")}
         />
       </div>
     </div>
